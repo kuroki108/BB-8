f9e193dea82aa2bd7038fd2da0b67e231aad4478
kuroki108 <finngindele07@gmail.com>
2026-02-11 22:22:17 +0100

    Remove Number Guessing function

--- bot.py
+++ bot.py
@@ -31,42 +31,6 @@ async def watch(ctx):
 
 
 
-@bot.command()
-async def play(ctx):
-    await ctx.send('Wanna play a number guessing game?')
-    await asyncio.sleep(1)
-    await ctx.send('Give me a Number from 1 to 10: ')
-    
-    number = random.randint(1, 10)
-    #await ctx.send(f'Solution: {number}')   // Show the solution for testing
-
-    def check(msg):
-        return msg.author == ctx.author and msg.channel == ctx.channel
-    
-    while True:
-        try:
-            guess_msg = await bot.wait_for('message', check=check)
-            guess = int(f'{guess_msg.content}')
-
-            if guess < number:
-                await ctx.send('Your Guess is too low. ')
-                await ctx.send('Try again!')
-            
-            elif guess > number:
-                await ctx.send('Your Guess is too high')
-                await ctx.send('Try again!')
-                
-            elif guess == number:
-                await ctx.send('Yaaay You won')
-                
-                break
-        except ValueError:
-            await ctx.send('Give me a valid Number from 1 to 10!')
-
-    await asyncio.sleep(1)    
-    await ctx.send('See you next time.')
-
-
 if __name__ == "__main__":
     try:
         bot.run(TOKEN)
